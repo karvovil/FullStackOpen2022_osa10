ed6362f61ff5403d044eb630ad37269d96955062
Ville Karvonen <villekarvonen1@gmail.com>
2022-08-30 13:16:49 +0300

    SignUp form visuals and validations added

--- rate-repository-app/src/components/SignUp.jsx
+++ rate-repository-app/src/components/SignUp.jsx
@@ -1,24 +1,77 @@
-import { StyleSheet, Text  } from 'react-native';
-import { Route, Routes, Navigate } from 'react-router-native';
-import RepositoryList from './RepositoryList';
-import AppBar from './AppBar';
-import SignIn from './SignIn';
-import SingleRepository from './SingleRepository';
-import Review from './Review';
+import { View, StyleSheet } from 'react-native';
+import FormikTextInput from './FormikTextInput';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { useNavigate } from "react-router-dom";
+import BigBlueButton from './BigBlueButton';
+import { CREATE_REVIEW } from '../graphql/mutations';
+import { useMutation } from '@apollo/client/react';
+
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    flexShrink: 1,
-    backgroundColor: '#e1e4e8'
+    flex: 0,
+    justifyContent: "flex-start",
+    backgroundColor: "#fff",
+    flexShrink: 0,
   },
 });
 
-const SignUp = () => {
+const initialValues = {
+  username: '',
+  password: '',
+  confirmation: '',
+};
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Username is required')
+    .min(1, 'Must be at least 1 long')
+    .max(30, 'Max 30 characters'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(5, 'Must be at least 5 long')
+    .max(30, 'Max 50 characters'),
+  confirmation: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords do not match')
+    .required('Password confirmation is required'),
+});
+
+const ReviewForm = ({ onSubmit }) => {
   return (
-    <Text>
-    Sign up
-    </Text>
+    <View style={styles.container}>
+      <FormikTextInput
+        name="username" placeholder="Username"
+      />
+      <FormikTextInput
+        name="password" placeholder="Password" secureTextEntry={true}
+      />
+      <FormikTextInput
+        name="confirmation"
+        placeholder="Password confirmation"
+        secureTextEntry={true}
+      />
+      <BigBlueButton buttonText='Sign Up' onSubmit={onSubmit}/>
+    </View>
   );
 };
 
+const SignUp = () => {
+  //const [mutate] = useMutation(CREATE_REVIEW);
+  //const navigate = useNavigate();
+
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}>
+      {({ handleSubmit }) => <ReviewForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+};
 export default SignUp;
